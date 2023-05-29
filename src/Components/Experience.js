afe251a47e56cb9e39f40c40dfbf4443035b4604
Mohamed Ashif Ali Jamal Mohamed <55224952+ashif1989@users.noreply.github.com>
2023-05-29 19:48:12 +0530

    Added Custom hooks reuse changes (#2)

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -8,18 +8,24 @@ const Experience = () => {
       info1: "DanskeIT",
       info2: "Chief Software Engineer",
       info3: "Bangalore",
+      info4:
+        "Working as Front End Full Stack Developer and working in different technologies like HTML, CSS, Javascript, React JS, .Net, Java",
       id: 1,
     },
     {
       info1: "HP",
       info2: "Technology Consultant",
       info3: "Bangalore",
+      info4:
+        "Worked as Java Full Stack Developer and working in different technologies like Java, Rest API, API Connect, Automation Testing, Mainframe.",
       id: 2,
     },
     {
       info1: "Patni",
       info2: "Senior Software Engineer",
       info3: "Mumbai",
+      info4:
+        "Worked as Senior Mainframe Developer and working in different technologies like REXX, JCL, COBOL, CICS.",
       id: 3,
     },
   ]);
--- src/Components/ExperienceDetails.js
+++ src/Components/ExperienceDetails.js
@@ -1,9 +1,26 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const ExperienceDetails = () => {
   const { id } = useParams();
+  const { status, error, loading } = useFetch(
+    "https://oltgg4-3000.csb.app/experienceDetails/" + id
+  );
+  console.log("loading", loading);
+  console.log("error", error);
+  //console.log("details", details);
 
-  return <div>Experience Details - {id}</div>;
+  return (
+    <>
+      {loading && <div>Experience Loading..</div>}
+      {error && <div>{error}</div>}
+      {status && (
+        <>
+          <div>{status}</div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ExperienceDetails;
